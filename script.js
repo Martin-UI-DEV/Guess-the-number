d7b4c5826611b2d4970688a049f7c01e6906f75c
Martin-UI-DEV <martingomez092@gmail.com>
2024-10-01 18:04:16 -0300

    return matches

--- script.js
+++ script.js
@@ -1,52 +1,97 @@
-const $ = (s) => document.querySelector(s);
-const $$ = (s) => document.querySelectorAll(s);
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
 const inputGuess = $('#guess');
-const submit = $('#check');
-const list = $('#list');
-const message = $('#message');
-// const randomNumber = Math.floor(Math.random() * 8999) + 1000;
-let randomNumber = 3456;
+const submitButton = $('#check');
+const resultList = $('#list');
+const messageDisplay = $('#message');
+
+let randomNumber = Math.floor(Math.random() * 8999) + 1000;
 inputGuess.maxLength = 4;
 
-check.addEventListener('click', validateNumber);
+submitButton.addEventListener('click', validateGuess);
 document.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') validateNumber()
+    if (event.key === 'Enter') validateGuess();
 });
 
-function arrayRandomNumber(num) {
+function convertNumberToArray(num) {
     return Array.from(String(num), Number);
 }
 
-function setValidity(x) { 
-    message.textContent = x;
-};
-
-function compareNumbers(num1, num2){
-    console.log(num1, num2);
+function setMessage(message) { 
+    messageDisplay.textContent = message;
 }
 
-function validateNumber() {
-    let guess = inputGuess.value;
+function countMatchingNumbers(arr1, arr2) {
+    const countMap1 = createCountMap(arr1);
+    const countMap2 = createCountMap(arr2);
+
+    let totalCount = 0;
+
+    countMap1.forEach((count, number) => {
+        if (countMap2.has(number)) {
+            totalCount += Math.min(count, countMap2.get(number));
+        }
+    });
+
+    return totalCount;
+}
+
+function createCountMap(arr) {
+    const countMap = new Map();
+    arr.forEach(num => {
+        countMap.set(num, (countMap.get(num) || 0) + 1);
+    });
+    return countMap;
+}
+
+function validateGuess() {
+    const guess = inputGuess.value;
+
     if (/[^0-9]/.test(guess))  {
-        setValidity('Please, enter only numbers')
+        setMessage('Por favor, ingrese solo números');
     } else if (guess.length !== 4) {
-        setValidity('Please, enter a 4-digit number')
+        setMessage('Por favor, ingrese un número de 4 dígitos');
     } else {
-        setValidity('');
-        const li = document.createElement('li');
-        li.textContent = guess + " -";
-        list.appendChild(li);
-        let arrayRandom = arrayRandomNumber(randomNumber);
-        let arrayGuess = arrayRandomNumber(guess);
-        compareNumbers(arrayRandom, arrayGuess)
-    }   
-}    
+        setMessage(''); 
+
+        const randomArray = convertNumberToArray(randomNumber);
+        const guessArray = convertNumberToArray(guess);
+        const matchingCount = countMatchingNumbers(randomArray, guessArray);
+        const commonIndexValue =  commonIndex(randomArray, guessArray);
+        displayResult(guess, matchingCount, commonIndexValue);
+        
+    }
+}
+
+function commonIndex(arr1, arr2) {
+    const commonIndices = [];
+    let commonIndex = 0;
+    arr1.forEach((num, index) => {
+        const indexInArr2 = arr2.indexOf(num);
+        if (indexInArr2 !== -1) {
+            commonIndices.push({ number: num, indexInArr1: index, indexInArr2: indexInArr2 });        
+        }
+        if (index === indexInArr2) {
+            commonIndex++
+        }
+    })
+    return commonIndex;
+}
 
 
+function displayResult(guess, matchingCount, commonIndexValue ) {
+    const listItem = document.createElement('li');
+    const paragraph = document.createElement('p');
 
+    const resultMessage = matchingCount === 1 
+        ? `${guess} - <span>${matchingCount}</span> Número coincide - <span>${commonIndexValue}</span> posiciones`
+        : `${guess} - <span>${matchingCount}</span> Números coinciden - <span>${commonIndexValue}</span> posiciones`;
 
+    paragraph.innerHTML = resultMessage;
+    listItem.appendChild(paragraph);
+    resultList.appendChild(listItem);
+}
 
-
-
-
+// const randomElement = $('#random');
+// randomElement.textContent = randomNumber;
